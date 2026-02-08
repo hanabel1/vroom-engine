9ed54d80f311a5d30172960bade8e4f01f0768af
Alexander Langolf <alexander.langolf@multiverse.io>
2026-02-08 15:11:22 +0000

    Fix

--- src/ui/components/HtmlPreview.tsx
+++ src/ui/components/HtmlPreview.tsx
@@ -10,10 +10,22 @@ export function HtmlPreview({ html, className, showDimensions = false, previewSt
     <div
       className={className ? `html-preview ${className}` : 'html-preview'}
       data-preview-state={previewState ?? undefined}
+      style={{ width: '100%', height: '100%', position: 'relative', display: 'flex' }}
     >
       <div
         className="html-preview-content"
         dangerouslySetInnerHTML={{ __html: html }}
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          overflow: 'hidden',
+          maxWidth: '100%',
+          maxHeight: '100%',
+          placeItems: 'center',
+        }}
       />
     </div>
   );
